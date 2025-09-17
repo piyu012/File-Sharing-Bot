1d858377482df83bb82498510404bd60bd37711d
piyu012 <pcdelegend@gmail.com>
2025-09-17 09:14:59 +0530

    update bot code

--- database/database.py
+++ database/database.py
@@ -1,42 +1,22 @@
-import pymongo, os
+import pymongo, certifi, dns.resolver, os
 from config import DB_URL, DB_NAME
 
-dbclient = pymongo.MongoClient(DB_URL)
-database = dbclient[DB_NAME]
-user_data = database['users']
+# Termux DNS fix
+dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers = ['8.8.8.8','1.1.1.1']
 
+dbclient = pymongo.MongoClient(DB_URL, tls=True, tlsCAFile=certifi.where(), serverSelectionTimeoutMS=30000)
+db = dbclient[DB_NAME]
+user_data = db["users"]
 
+async def add_user(user_id:int):
+    user_data.update_one({"_id": user_id}, {"$setOnInsert":{"_id": user_id}}, upsert=True)
 
-async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
-    return bool(found)
+async def del_user(user_id:int):
+    user_data.delete_one({"_id": user_id})
 
-async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
-    return
+async def present_user(user_id:int) -> bool:
+    return user_data.find_one({"_id": user_id}) is not None
 
 async def full_userbase():
-    user_docs = user_data.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
-        
-    return user_ids
-
-async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
-    return
-
-
-
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+    return [u["_id"] for u in user_data.find({}, {"_id":1})]
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from bot import Bot
+import dns.resolver
+dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers = ['8.8.8.8', '1.1.1.1']
+
 
 Bot().run()
