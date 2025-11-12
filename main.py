b6b9041ce02b678654a82a6dd8b2b719118a22fd
piyu012 <pcdelegend@gmail.com>
2025-11-12 21:08:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from bot 
-from api import Bot
+from bot import Bot
+from api 
 import dns.resolver import setup_routes
 dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
 dns.resolver.default_resolver.nameservers = ['8.8.8.8', '1.1.1.1']
