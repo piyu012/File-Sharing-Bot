c69161b8d73d217acdfe69ec6838d9ee8a168459
piyu012 <pcdelegend@gmail.com>
2025-11-12 20:03:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from bot import Bot
-import dns.resolver
+from bot from api import Bot
+import dns.resolver import setup_routes
 dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
 dns.resolver.default_resolver.nameservers = ['8.8.8.8', '1.1.1.1']
 
+app = web.AppRunner(await web_server())
+await app.setup()
+site = web.TCPSite(app, "0.0.0.0", PORT)
 
 Bot().run()
